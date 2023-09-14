1801342e4c6d0d1a6fd96ff129a8f2df25c83a6c
Bar Ilan, Yehonathan <yb448h@intl.att.com>
2023-09-14 16:43:19 +0300

    [00004] - update lab 4 and 5...

--- perf_hooks_lab/index.js
+++
@@ -1,5 +0,0 @@
-const { performance } = require('perf_hooks');
-const start = performance.now();
-// Code section to measure the performance
-const end = performance.now();
-console.log(`Elapsed time: ${(end-start).toFixed(2)} milliseconds`);
