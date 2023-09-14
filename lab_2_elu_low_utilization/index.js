f9bdd92dbd293a23aff5ce1be12162368c70fef7
Bar Ilan, Yehonathan <yb448h@intl.att.com>
2023-09-14 15:24:00 +0300

    [00004] - update lab 2...

--- lab_2_elu_low_utilization/index.js
+++ lab_2_elu_low_utilization/index.js
@@ -1,13 +1,14 @@
 const { performance } = require('perf_hooks');
 const { eventLoopUtilization } = require('node:perf_hooks').performance;
-setImmediate(() => {
+const awaitTimeout = delay =>
+  new Promise(resolve => setTimeout(resolve, delay));
+
+setImmediate(async () => {
 	const start = performance.now();
 	const elu = eventLoopUtilization();
 	let sum = 0;
 	for (let i = 0; i < 100; i++) {
-		setTimeout(function() {
-		  sum = sum +1;
-		}, 60);
+		await awaitTimeout(60).then(sum = sum +1);
 	}
 	const end = performance.now();
 	console.log(`ELU utilization: ${eventLoopUtilization(elu).utilization}`);
