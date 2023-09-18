1334ad63f285488edabcf93dfd5a7c96df442655
Bar Ilan, Yehonathan <yb448h@intl.att.com>
2023-09-18 12:26:03 +0300

    [00004] - format and prepare for review...

--- lab_2_elu_low_utilization/index.js
+++ lab_2_elu_low_utilization/index.js
@@ -1,16 +1,16 @@
-const { performance } = require('perf_hooks');
-const { eventLoopUtilization } = require('node:perf_hooks').performance;
+const {performance} = require('perf_hooks');
+const {eventLoopUtilization} = require('node:perf_hooks').performance;
 const awaitTimeout = delay =>
-  new Promise(resolve => setTimeout(resolve, delay));
+    new Promise(resolve => setTimeout(resolve, delay));
 
 setImmediate(async () => {
-	const start = performance.now();
-	const elu = eventLoopUtilization();
-	let sum = 0;
-	for (let i = 0; i < 100; i++) {
-		await awaitTimeout(60).then(sum = sum +1);
-	}
-	const end = performance.now();
-	console.log(`ELU utilization: ${eventLoopUtilization(elu).utilization}`);
-	console.log(`Elapsed time: ${(end-start).toFixed(2)} milliseconds`);
+    const start = performance.now();
+    const elu = eventLoopUtilization();
+    let sum = 0;
+    for (let i = 0; i < 100; i++) {
+        await awaitTimeout(60).then(sum = sum + 1);
+    }
+    const end = performance.now();
+    console.log(`ELU utilization: ${eventLoopUtilization(elu).utilization}`);
+    console.log(`Elapsed time: ${(end - start).toFixed(2)} milliseconds`);
 }); 
--- lab_4_slow_event_loop/index.js
+++ lab_4_slow_event_loop/index.js
@@ -1,22 +1,22 @@
 'use strict'
 
-const restify = require('restify')
-const server = restify.createServer()
+const restify = require('restify');
+const server = restify.createServer();
 
-function sleep (ms) {
-  const future = Date.now() + ms
-  while (Date.now() < future);
+function sleep(ms) {
+    const future = Date.now() + ms;
+    while (Date.now() < future) ;
 }
 
 server.get('/', function (req, res, next) {
-  sleep(30)
-  res.send({})
-  next()
+    sleep(30);
+    res.send({});
+    next();
 })
 
 server.listen(3000)
 
 process.on('SIGINT', function () {
-  console.error('Caught SIGINT, shutting down.')
-  server.close()
+    console.error('Caught SIGINT, shutting down.');
+    server.close();
 })
