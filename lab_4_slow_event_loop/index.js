fbd2174ae75d41f6f575cb7bae0ba5f38ce44a77
Bar Ilan, Yehonathan <yb448h@intl.att.com>
2023-09-18 12:19:15 +0300

    [00004] - update lab 4 and 5...

--- lab_4_slow_event_loop/index.js
+++ lab_4_slow_event_loop/index.js
@@ -2,26 +2,16 @@
 
 const restify = require('restify')
 const server = restify.createServer()
-const async = require('async')
 
-function awaitData (callback) {
-  async.series([
-    (done1) => setTimeout(done1, Math.random() * 1000),
-    (done1) => async.parallel([
-      (done2) => setTimeout(done2, Math.random() * 1000),
-      (done2) => setTimeout(done2, Math.random() * 1000),
-      (done2) => setTimeout(done2, Math.random() * 1000),
-      (done2) => setTimeout(done2, Math.random() * 1000),
-      (done2) => setTimeout(done2, Math.random() * 1000)
-    ], done1)
-  ], callback)
+function sleep (ms) {
+  const future = Date.now() + ms
+  while (Date.now() < future);
 }
 
 server.get('/', function (req, res, next) {
-  awaitData(function () {
-    res.send({})
-    next()
-  })
+  sleep(30)
+  res.send({})
+  next()
 })
 
 server.listen(3000)
